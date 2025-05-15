143601db729b781a0619522d14b6be1c38055ebf
joe2sure <joe2sure1@gmail.com>
2025-05-15 12:54:55 +0100

    first commit

---
+++ src/app/home/page.js
@@ -0,0 +1,27 @@
+import FooterEight from "~/components/Section/Common/FooterEight";
+import HeaderSix from "~/components/Section/Common/Header/HeaderSix";
+import HeroSection from "~/components/Section/Home-8/HeroSection";
+import ItSolutionSection from "~/components/Section/Home-8/ItSolutionSection";
+import LetsBuildSection from "~/components/Section/Home-8/LetsBuildSection";
+import PricingSection from "~/components/Section/Home-8/PricingSection";
+import RecentBlogSection from "~/components/Section/Home-8/RecentBlogSection";
+import RecentProjectsSection from "~/components/Section/Home-8/RecentProjectsSection";
+import ServiceSection from "~/components/Section/Home-8/ServiceSection";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <ServiceSection />
+      <ItSolutionSection />
+      <RecentProjectsSection />
+      <LetsBuildSection />
+      <PricingSection />
+      <RecentBlogSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
